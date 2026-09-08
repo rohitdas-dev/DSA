027e6b3d13e0ae0639eed70f213532f89ba65d95
Rohit Das <rohitdasbelda68@gmail.com>
2026-09-09 00:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.11%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -17,6 +17,7 @@ class Solution:
         slow = head
         fast = head
         while fast != None and fast.next != None:
-            slow = slow.next
+            #slow = slow.next
             fast = fast.next.next
+            slow = slow.next
         return slow
